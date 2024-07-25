00833bb2497c1fe7bad85d89880a3c8575e1c7c3
Samuel Ume <cume357@gmail.com>
2024-07-25 21:12:10 +0100

    feature: added active navlink tracker

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,7 +1,13 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
+ 
 
 export default function Navbar() {
+  const pathname = usePathname()
+
+  const isActive = (href:string) => pathname === href
   return (
     <>
       <nav className="w-full px-16 py-4 grid-cols-10 justify-between shadow items-center hidden md:grid">
@@ -11,22 +17,23 @@ export default function Navbar() {
               <Image
                 src="/assets/OstroHublogo.png"
                 alt="logo"
-                width={130}
-                height={37}
+                width={170}
+                height={50}
+
               />
             </Link>
           </div>
         </div>
         <div className="col-span-5 col-end-11 items-center justify-center ">
-          <ul className="flex px-4  w-full justify-around text-lg items-center">
+          <ul className="flex px-4  w-full justify-around text-lg items-center font-semibold">
             <li>
-              <Link href={"/"}>Home</Link>
+              <Link href={"/"} className={isActive('/') ? "text-[#01AD4F]": ""}>Home</Link>
             </li>
             <li>
-              <Link href={"/"}>About</Link>
+              <Link href={"/about"} className={isActive('/about') ? "text-[#01AD4F]": ""}>About</Link>
             </li>
             <li>
-              <Link href={"/contact"}>Contact</Link>
+              <Link href={"/contact"} className={isActive('/contact') ? "text-[#01AD4F]": ""}>Contact</Link>
             </li>
             <li className="w-14 h-7 bg-[#14141411] rounded-2xl text-right grid justify-end">
               <span className="w-7 h-7 rounded-full bg-black-900 block text-right"></span>
