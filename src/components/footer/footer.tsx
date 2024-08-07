f7945cfc586ba56e89ff3e09e065d4042216f188
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2024-08-07 11:27:24 +0100

    some style modifications to the footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Typography from "../typography";
 import { FooterLinks } from "@/constant";
 import Link from "next/link";
+// import { Icon } from "@iconify/react";
 import { Icon } from "@iconify/react";
 
 interface DropDownStates {
