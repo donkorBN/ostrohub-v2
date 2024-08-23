38ffd4a9b6d04520ac4c2d258837c8d51c5f24d6
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2024-08-23 11:54:51 +0100

    feat: add context to next-config for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 // import babelPluginMacros from 'babel-plugin-macros';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  distDir: "dist"
+};
 
 export default nextConfig;
